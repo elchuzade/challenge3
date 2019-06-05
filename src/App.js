c42b4d41d464f2c64c9c2d7b1e027cb26df62f29
Kamran Elchuzade <elchuzade@gmail.com>
2019-06-05 14:31:02 +0300

    redesigned added new library and added fetch user info from github with error handling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import UserInformation from './UserInformation';
+import logo from '../public/spy.svg';
+import classnames from 'classnames';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { user: {} };
+    this.state = {
+      user: {},
+      errors: {},
+      fetched: false
+    };
+    // for username min is 1 character and max is 39 characters according to the GitHub signin rules
   }
-
-  getUserInformation() {
+  onChange = e => {
+    e.preventDefault();
+    if (this.state.errors.usernameError) {
+      const updatedErrors = this.state.errors;
+      delete updatedErrors.usernameError;
+      this.setState({ errors: updatedErrors });
+    }
+  };
+  getUserInformation = () => {
     /*
       TODO: fetch a user from the GitHub API
 
@@ -18,25 +31,60 @@ class App extends Component {
          fetching resources (including across the network).
        2) Maybe you want to update the state here.
     */
-  }
+    const name = this.refs.name.value;
+    const errors = {};
+    if (name.length < 1 || name.length > 39) {
+      errors.usernameError = 'Username must be between 1 and 39 characters';
+      this.setState({ errors });
+    } else {
+      fetch(`http://api.github.com/users/${name}`).then(response => {
+        if (response.status === 200) {
+          response.json().then(data => {
+            this.setState({ fetched: true, user: data, errors: {} });
+          });
+        } else {
+          this.setState({ errors });
+          errors.responseError = response.statusText;
+        }
+      });
+    }
+  };
 
   render() {
-    const { user } = this.state;
+    const { user, errors } = this.state;
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} alt="logo" className="mainLogo" />
+          <h2>Welcome to GitHub Spy</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro mt-3 lead">
+          If GitHub is prohibited in your country, you can type a GitHub
+          username here and check all the repositories
         </p>
-        <div className="App-intro">
+        <div className="App-intro container">
           <hr />
-          <p>Click on the button to fetch the user information</p>
-          <button onClick={this.getUserInformation.bind(this)}>Click me</button>
+          <div className="col-6 mx-auto mb-3">
+            <input
+              onChange={this.onChange}
+              type="text"
+              className={classnames('form-control', {
+                'is-invalid': errors.usernameError
+              })}
+              ref="name"
+              placeholder="Enter GitHub Username"
+            />
+            {this.state.errors.usernameError && (
+              <div className="invalid-feedback">
+                {this.state.errors.usernameError}
+              </div>
+            )}
+          </div>
+          <button className="btn btn-primary" onClick={this.getUserInformation}>
+            Show Repos
+          </button>
         </div>
-        <UserInformation />
+        <UserInformation user={user} errors={errors} />
       </div>
     );
   }
